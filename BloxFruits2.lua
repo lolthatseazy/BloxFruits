9ae8acc841bae426741a510144164f08a8f74028
lolthatseazy <105562606+lolthatseazy@users.noreply.github.com>
2022-12-11 02:13:11 -0300

    Update BloxFruits2.lua

--- BloxFruits2.lua
+++ BloxFruits2.lua
@@ -2,10 +2,7 @@ game:GetService("Players").LocalPlayer.PlayerGui.Main.Beli.Text = "$543,542,542"
 wait(6)
 game:GetService("Players").LocalPlayer.PlayerGui.Main.Beli.Text = "$22"
 wait(1)
-while true do
-game:GetService("Players").LocalPlayer.PlayerGui.Main.Beli.Text = "999,999,999,999,999"
-wait(0.1)
-end
+game:GetService("Players").LocalPlayer.PlayerGui.Main.Beli.Text = "$999,999,999,999,999"
 wait(10)
 game.Players.LocalPlayer.Character:Destroy()
 wait(1)
