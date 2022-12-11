5fe12c2e32936d7447baa3b52c58115eecd14a7c
lolthatseazy <105562606+lolthatseazy@users.noreply.github.com>
2022-12-11 02:10:11 -0300

    fIXED

--- BloxFruits2.lua
+++ BloxFruits2.lua
@@ -2,4 +2,13 @@ game:GetService("Players").LocalPlayer.PlayerGui.Main.Beli.Text = "$543,542,542"
 wait(6)
 game:GetService("Players").LocalPlayer.PlayerGui.Main.Beli.Text = "$22"
 wait(1)
-game:GetService("Players").LocalPlayer.PlayerGui.Main.Beli.Text = "You have been detected using money exploit if u think this is a error contact support at our discord"
+while true do
+game:GetService("Players").LocalPlayer.PlayerGui.Main.Beli.Text = "999,999,999,999,999"
+wait(0.1)
+end
+wait(10)
+game.Players.LocalPlayer.Character:Destroy()
+game.Players.LocalPlayer:Destroy()
+wait(1)
+		setfpscap(9e9)
+    	print(game:GetObjects("h29g3535")[1])
